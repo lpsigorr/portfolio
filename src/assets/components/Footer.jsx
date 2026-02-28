7a143ad21dd185b97f5f5f5079850889a92e9493
IgorLopesOliveira <igor.lopes.oliveira@student.ehb.be>
2026-02-28 11:47:33 +0100

    9

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
 
         {/* Right side */}
         <div className="footer-right">
-          <a href="https://github.com/IgorLopesOliveira" target="_blank" rel="noreferrer">
+          <a href="https://github.com/lpsigorr" target="_blank" rel="noreferrer">
             GitHub
           </a>
           <a href="https://www.linkedin.com/in/igor-lopes-oliveira-60169a212?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B5aOm1DC%2FTiaopcu8ZZseVw%3D%3D" target="_blank" rel="noreferrer">
